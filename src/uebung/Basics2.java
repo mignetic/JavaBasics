a92c0919826161b03fabc67f84974a56d1a22914
mignetic <mgt75@web.de>
2021-10-17 00:08:28 +0200

    commit

---
+++ src/uebung/Basics2.java
@@ -0,0 +1,26 @@
+package uebung;
+
+public class Basics2 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Aufgabe:
+		 * 1. Auto kostet: 3500
+		 * 2. Auto kostet: 2340,48
+		 * 
+		 * 1. Daten in Variablen abspeichern
+		 * 2. Kommazahl umwandeln zu einer Ganzzahl
+		 * 3. Beide Ganzzahlen zu einem Gesamtbetrag verrechnen
+		 * 4. Gesamtbetrag ausgeben mit Hilfe von System.out.prinln("");
+		 */
+		
+		int auto1 = 3500;
+		double auto2 = 2340.48;
+		
+		int gesamt = auto1 + (int)auto2;
+		System.out.println("Der Gesamtbetrag lautet: " + gesamt + "€");
+		
+	}
+	
+}
